4164925aaa40a75c65bac662439a602db3242989
Caio Neves <ncaio037@gmail.com>
2023-04-26 00:35:41 -0300

    creating the dockerfile with the base application images.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import 'dotenv/config'
 import { sendEmailModule } from './sendEMail/sendemail.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 const MONGO_URI = process.env.MONGO_URI
@@ -13,7 +14,8 @@ const MONGO_URI = process.env.MONGO_URI
   imports: [
     MongooseModule.forRoot(MONGO_URI), 
     UsersModule,
-    sendEmailModule
+    sendEmailModule,
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
